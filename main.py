20a632232e9de951605a09c59dd2fa1a9cde33ea
Emre Duman <76225090+Emre1Duman@users.noreply.github.com>
2023-10-21 11:42:56 +0100

    Implemented Timer & Countdown mechanism

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -14,7 +15,20 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5 * 60)
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -22,22 +36,26 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+
+
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 25, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 50))
 canvas.grid(column=1, row=1)
+
+
 
 title = Label(text="Timer", font=(FONT_NAME, 50, "bold"), fg=GREEN, bg=YELLOW)
 title.grid(column=1, row=0)
 
-start = Button(text="Start", highlightbackground=YELLOW)
+start = Button(text="Start", highlightbackground=YELLOW, highlightthickness=0, command=start_timer)
 start.grid(column=0, row=2)
 
-reset = Button(text="Reset", highlightbackground=YELLOW)
+reset = Button(text="Reset", highlightbackground=YELLOW, highlightthickness=0)
 reset.grid(column=2, row=2)
 
-check_mark = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
-check_mark.grid(column=1, row=3)
+check_marks = Label(text="✓", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, "bold"))
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
